c55257b20c7ec4d11fd6dcdecdda86d6179e07c7
Cyan75 <incanto075@gmail.com>
2021-12-01 00:25:03 +0900

    day1

---
+++ ModernC++Intro/ModernC++Intro/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ModernC++Intro
+//
+//  Created by Cyan on 2021/11/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
