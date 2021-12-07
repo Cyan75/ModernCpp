95a655bd9b58dff910628e8d3526f6c8013dffa2
Cyan75 <incanto075@gmail.com>
2021-12-08 00:33:19 +0900

    day3 pointers and references

--- ModernC++Intro/ModernC++Intro/main.cpp
+++ ModernC++Intro/ModernC++Intro/main.cpp
@@ -12,14 +12,21 @@ void AddVal(int *a, int *b, int *result); //Add two numbers and return the sum t
 void Swap(int *a, int *b) ;  //Swap the value of two integers
 void Factorial(int *a, int *result);       //Generate the factorial of a number and return that through the second pointer argument
 
+void Add(int a,int b, int &result) ;    //Add two numbers and return the result through a reference parameter
+void Factorial(int a, int &result) ;    //Find factorial of a number and return that through a reference parameter
+void Swap(int &a, int &b) ;            //Swap two numbers through reference arguments
+
 int main(int argc, const char * argv[]) {
+    /*
     int a = 3;
     int *pa = &a;
     int b = 5;
     int *pb = &b;
     int c = 0;
     int *pc = &c;
+    */
     
+    /*
     std::cout << "Hello, World!\n"<< "initially : " << "a : "<< a <<"\n"<< "b : "<<b<<std::endl;
     std::cout << "Add : " << Add(pa, pb) << std::endl;
     AddVal(pa, pb, pc);
@@ -28,6 +35,43 @@ int main(int argc, const char * argv[]) {
     std::cout << "Swap , a:  " << *pa <<"\nb : "<< *pb << std::endl;
     Factorial(pa, pc);
     std::cout << "Factorial , c:  " << *pc << std::endl;
+    */
+    
+    // Const Pointer and Reference
+    //Try to modify x1 & x2 and see the compilation output
+    
+    int x = 5;
+    const int MAX = 12;
+    int &ref_x1 = x;
+    const int &ref_x2 = x;
+    
+    //ref_x1 = 2; // OK
+    //ref_x2 = 2; // Not OK
+    
+    //Try to modify the pointer (e.g. ptr1) and the pointee value (e.g. *ptr1)
+    
+    const int* ptr1 = &x;   // a pointer to a constant int
+    int* const ptr2 = &x;   // a constant pointer to an int
+    const int* const ptr3 = &x; // a constant pointer to a constant int
+    
+    //*ptr1 = 10; // Not OK, the content is const
+    //*ptr2 = 10; // OK
+    //*ptr3 = 11; // Not OK, the content is const
+    
+    //Find which declarations are valid. If invalid, correct the declaration
+//    const int *ptr3 = &MAX;
+    //int * ptr4 = &MAX; // should be const int*
+    
+//    const int &r1 = ref_x1;
+//    const int &r2 = ref_x2;   // should be const int
+    
+    
+//    int *&p_ref1 = ptr1;  //cannot modify const
+//    *ptr1 = 10;
+//    const int *&p_ref2 = ptr2; //Not OK, should be int * const
+//    *ptr2 = 10;
+    
+    std::cout<< " x = "<< x << std::endl;
     
     return 0;
 }
@@ -53,6 +97,26 @@ void Factorial(int *a, int *result){
         *result *= i;
         i--;
     }
+}
+
+void Add(int a,int b, int &result){
+    int &refa = a;
+    int &refb = b;
+    result = refa + refb;
+}
+void Factorial(int a, int &result) {
+    int &ref = a;
+    int i = ref;
+    while(i > 1){
+        result *=i;
+        i--;
+    }
+}
+
+void Swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 
