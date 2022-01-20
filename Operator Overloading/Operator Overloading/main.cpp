ad7a7d2eb5a1232ca9538f88d4cccd02864fdbfe
Cyan75 <incanto075@gmail.com>
2022-01-21 00:36:16 +0900

    day19 smart pointers

--- Operator Overloading/Operator Overloading/main.cpp
+++ Operator Overloading/Operator Overloading/main.cpp
@@ -27,18 +27,37 @@ public:
         return *m_p;
     }
 };
+void Process(Integer val) {
+    
+}
+
+void Process(std::unique_ptr<Integer> ptr){
+    std::cout << ptr->GetValue() << std::endl;
+}
+
+void Process(std::shared_ptr<Integer> ptr){
+    std::cout << ptr->GetValue() << std::endl; //ref count of shared pointer incremented
+} //ref count decremented when ptr is destroyed
+
 void CreateInteger() {
     IntPtr p1 = new Integer(5);
-    *p1->SetValue(4);
+    (*p1).SetValue(4);
+    p1->SetValue(3);
     
-    std::unique_ptr<Integer> p(new Integer);
+     std::unique_ptr<Integer> p(new Integer);
     //auto p2(p);
-    (*p).SetValue(3);
+    (*p).SetValue(2);
     //std::cout << p->GetValue() << std::endl;
-}
-void Process(Integer val) {
-
-}
+//    Process(p);// won't work cannot copy a unique pointer
+    Process(std::move(p));  //unique pointer can be moved
+    
+    std::shared_ptr<Integer> ps(new Integer);
+    (*ps).SetValue(10);
+    Process(ps);    //ref count of shared pointer incremented
+    std::cout << ps->GetValue() << std::endl; 
+    
+} // the shared pointer ps is destroyed at the end of the scope
+// the reference count is decremented to 0, the underlying resource will be released
 
 int main(int argc, const char * argv[]) {
 //    Integer a(3);
