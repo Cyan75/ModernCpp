e75ce039130a3acb58a4065e27ea228d43a04882
Cyan75 <incanto075@gmail.com>
2022-01-24 23:55:23 +0900

    day20 type conversion

--- Operator Overloading/Operator Overloading/main.cpp
+++ Operator Overloading/Operator Overloading/main.cpp
@@ -54,16 +54,40 @@ void CreateInteger() {
     std::shared_ptr<Integer> ps(new Integer);
     (*ps).SetValue(10);
     Process(ps);    //ref count of shared pointer incremented
-    std::cout << ps->GetValue() << std::endl; 
+    std::cout << ps->GetValue() << std::endl;
     
 } // the shared pointer ps is destroyed at the end of the scope
 // the reference count is decremented to 0, the underlying resource will be released
 
+void Print(Integer a){
+    std::cout<<a.GetValue()<<std::endl;
+}
+
+//void Print(const Integer &a){
+//    std::cout<<a.GetValue()<<std::endl;
+//}
+
 int main(int argc, const char * argv[]) {
-//    Integer a(3);
     /*auto b(std::move(a));
     std::cout << a << std::endl;*/
 //    Process(std::move(a));
-    CreateInteger();
+//    CreateInteger();
+    
+    /*
+    int a = 5, b = 3;
+    float f = static_cast<float>(a)/b;
+//    char *p = static_cast<char*>(&a);// not working
+    // what is going to be done : cast a pointer to an integer to a pointer to a char
+    char *p = reinterpret_cast<char*>(&a);
+    const int x = 1;
+    int *ip = const_cast<int*>(&x);
+    */
+    Integer a1(3);
+    Integer a2 = 5;
+    
+    Print(5);// Integer i(5) is invoked
+    
+    a1 = 7; //invokes the move assignment Integer & operator =(Integer &&a)
+    
     return 0;
 }
