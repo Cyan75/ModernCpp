5f19555960c970739453912869cc3c79d395626f
Cyan75 <incanto075@gmail.com>
2022-02-06 23:52:41 +0900

    day26 make() functions

--- MemoryManagement_Part2/MemoryManagement_Part2/source.cpp
+++ MemoryManagement_Part2/MemoryManagement_Part2/source.cpp
@@ -55,6 +55,13 @@ void SharedPtrDeleters(){
     std::cout << *p << '\n';
 }
 
+class Point{
+public:
+    Point(int x, int y){
+        
+    }
+};
+
 int main(){
     // unique pointer
     //    std::unique_ptr<int, Free> p {(int*) malloc(4), Free()};
@@ -76,5 +83,33 @@ int main(){
 //    delete emp;
 //    delete prj;
  */
+    /*
+    std::unique_ptr<int> p1{new int[5]{1,2,3,4,5}};
+//    p1[0] = 10; // smart pointer of a primitive type does not support this.
+    p1.get()[0] = 10; // this is OK but it does not delete properly
+    
+    // use this
+    std::unique_ptr<int[]> p2{new int[5]{1,2,3,4,5}};
+    p2[0] = 10;
+    
+    //for shared pointer, C++17 or later supports
+    std::shared_ptr<int[]> p3{new int[5]{1,2,3,4,5}};
+    p3[0] = 10;
+    */
+    // construct without using new
+    {
+        //    std::unique_ptr<int[]> p{new int[5]};
+        auto p = std::make_unique<int>(5);
+        auto pt = std::make_unique<Point>(3,5);
+        auto pArr = std::make_unique<int[]>(5);
+        pArr[0] = 0;
+    }
+    {
+        auto p = std::make_shared<int>(5);
+        auto pt = std::make_shared<Point>(3,5);
+//        auto pArr = std::make_shared<int[]>(5);   //C++20
+        pArr[0] = 0;
+    }
+
     return 0;
 }
