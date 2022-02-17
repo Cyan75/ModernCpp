b73fb9bb1b2c78dc0eac0a248ba793d01db8ede5
Cyan75 <incanto075@gmail.com>
2022-02-17 22:42:52 +0900

    day28 std::string

--- C++Goodies/C++Goodies/main.cpp
+++ C++Goodies/C++Goodies/main.cpp
@@ -7,7 +7,7 @@
 
 #include <iostream>
 
-
+/*
 enum class Colour{RED, GREEN, BLUE};//global scope
 
 void FillColour(Colour colour){
@@ -25,14 +25,54 @@ void FillColour(Colour colour){
         std::cout<< " BLUE " << std::endl;
     }
 }
+*/
+void UsingStdString(){
+    //String
+    //Initialisation and assign
+    std::string s = "Hello";
+    
+    //Access
+    s[0] = 'W';
+    char ch = s[1];
+    std::cout<<s<<std::endl;
+//    std::cin>s;
+    std::getline(std::cin, s);
+    
+    //Size
+    s.length();
+    
+    //Insert & concatenate
+    std::string s1{"hello"}, s2{"world"};
+    s = s1 + s1;
+    s+=s1;
+    s.insert(6,"world");
+    
+    //Comparison
+    if (s1 != s2){
+        
+    }
+    //Removal
+    s.erase();
+    s.erase(0,5);
+    s.clear();
+    
+    //Search
+    auto pos = s.find("world", 0);  //substring
+    if (pos != std::string::npos){
+        //found
+    }
+}
 
 enum class TrafficLight : char{RED, GREEN, YELLOW};
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    /*
     std::cout << "Hello, World!\n";
     Colour c = Colour::RED;
     FillColour(c);
     FillColour(Colour::GREEN);
     FillColour(static_cast<Colour>(2));
+    */
+    //TODO lower to upper upper to lower translation using ToUpper and ToLower
+    
     return 0;
 }
