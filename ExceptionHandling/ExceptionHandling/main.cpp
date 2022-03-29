9b1e09a609108df7d20e7b031ce2b5ac83a4d5e6
Cyan75 <incanto075@gmail.com>
2022-03-30 00:25:36 +0900

    day38 exception handling with constructors and destructors

--- ExceptionHandling/ExceptionHandling/main.cpp
+++ ExceptionHandling/ExceptionHandling/main.cpp
@@ -9,16 +9,58 @@
 #include <memory>
 #include <vector>
 #include <random>
-class Test{
+
+class A{
 public:
-    Test(){
-        std::cout<<"Test() : Acquire resources"<<std::endl;
+    A(){
+        std::cout << "A()" <<std::endl;
     }
-    ~Test(){
-        std::cout<<"~Test() : Release resources"<<std::endl;
+    ~A(){
+        std::cout<< "~A()"<<std::endl;
     }
 };
 
+class B{
+public:
+    B(){
+        std::cout << "B()" <<std::endl;
+    }
+    ~B(){
+        std::cout<< "~B()"<<std::endl;
+    }
+};
+
+class Test{
+//    A *pA{};
+//    B b{};
+//    int *pInt{};
+//    char *pStr{};
+//    int *pArr{};
+    std::unique_ptr<A> pA{};
+    B b{};
+    std::unique_ptr<int> pInt{};
+    std::unique_ptr<char> *pStr{};
+    std::vector<int> *pArr{};
+public:
+    Test(){
+        std::cout<<"Test() : Acquire resources"<<std::endl;
+        pA.reset(new A);
+        throw std::runtime_error("Failed to initialise");
+//        pA = new A;
+//        pInt = new int;
+//        pStr = new char[1000];
+//        pArr = new int[50000];
+    }
+    ~Test(){
+        std::cout<<"~Test() : Release resources"<<std::endl;
+        throw std::runtime_error("Failed to release");
+//        delete pA;
+//        delete pInt;
+//        delete pStr;
+//        delete pArr;
+    }
+};
+/*
 void ProcessRecords(int count){
 //    Test t;
 //    Test* pTest = new Test;
@@ -71,9 +113,10 @@ void ProcessRecords(int count){
         }
     }
 }
-
+*/
 int main(int argc, const char * argv[]) {
-    try {
+    
+    /*try {
 //        ProcessRecords(std::numeric_limits<int>::max());
         ProcessRecords(10);
     }
@@ -95,5 +138,12 @@ int main(int argc, const char * argv[]) {
     }
     
 //    std::cout<<ProcessRecords(std::numeric_limits<int>::max())<<std::endl;
+     */
+    try {
+        Test t;
+        throw std::runtime_error("Exception");
+    } catch (std::runtime_error &ex) {
+        std::cout << ex.what() <<std::endl;
+    }
     return 0;
 }
