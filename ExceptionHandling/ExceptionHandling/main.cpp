b9bd580c9e947e041c75528bbb6c157b1f61cb09
Cyan75 <incanto075@gmail.com>
2022-03-29 23:48:56 +0900

    day38 nested exception handling

--- ExceptionHandling/ExceptionHandling/main.cpp
+++ ExceptionHandling/ExceptionHandling/main.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <random>
 class Test{
 public:
     Test(){
@@ -19,14 +20,14 @@ public:
 };
 
 void ProcessRecords(int count){
-    Test t;
+//    Test t;
 //    Test* pTest = new Test;
     // smart ptr : not a pointer but an object
     std::unique_ptr<Test> uniqueTest (new Test);
     if(count<10){
         throw std::out_of_range("Count should not be greater than 10" );
     }
-    int * p = new int[count];
+//    int * p = new int[count];
 //    int *pArray = (int*)malloc(count * sizeof(int));
 //    if (pArray == nullptr){
 ////        std::cout<< "Failed to allocate memory" << std::endl;
@@ -45,14 +46,36 @@ void ProcessRecords(int count){
             vArr.push_back(i);
         }
 //    free(pArray);
-    delete[] p;
+//    delete[] p;
 //    delete pTest;
+    std::default_random_engine eng;
+    std::bernoulli_distribution dist;
+    int errors{};
+    for ( int i = 0; i< count; ++i) {
+        try{
+            std::cout<< "Processing record # : " << i << " ";
+            if(!dist(eng)){
+                ++errors;
+                throw  std::runtime_error("Could not process the record");
+            }
+            std::cout<<std::endl;
+        }
+        catch (std::runtime_error &ex){
+            std::cout<<"[Error "<<ex.what() << "]" << std::endl;
+            if(errors>4){
+                std::runtime_error err("Too many errors. Abandoning operation");
+                ex = err;
+                //re throw exception
+                throw;
+            }
+        }
+    }
 }
 
 int main(int argc, const char * argv[]) {
     try {
 //        ProcessRecords(std::numeric_limits<int>::max());
-        ProcessRecords(5);
+        ProcessRecords(10);
     }
 //    catch (std::runtime_error &ex) {
 //        std::cout << ex.what()<<std::endl;
